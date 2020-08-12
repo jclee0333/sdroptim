a2dc5611beae89b87870452881253ac30269de0a
jclee0333 <jclee0333@gmail.com>
2020-08-12 11:25:38 +0900

    Update PythonCodeModulator.py
    mistypo correction

--- sdroptim/PythonCodeModulator.py
+++ sdroptim/PythonCodeModulator.py
@@ -757,7 +757,7 @@ def getObjectiveFunction_(resources, gui_params, indirect=False, stepwise=False,
             rval_score_str = "confidence"
         #
         model_name = "model" if each_algorithm == 'DL_Pytorch' else "clf"
-        rval_each_algorithm = "sdroptim.retrieve_model(altorithm_name, "+model_name+", trial.number, "+rval_score_str + \
+        rval_each_algorithm = "sdroptim.retrieve_model(algorithm_name, "+model_name+", trial.number, "+rval_score_str + \
                               ", top_n_all = "+ str(top_n_all)+", top_n_each_algo = " + str(top_n_each_algo) + ")\n"
         rval_each_algorithm+= "return "+rval_score_str
         #
