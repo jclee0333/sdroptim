91b4a6353e4bb1adc425279af64857f4cdaa3984
jclee0333 <jclee0333@gmail.com>
2020-08-12 11:20:12 +0900

    Update __init__.py

--- sdroptim/__init__.py
+++ sdroptim/__init__.py
@@ -1,3 +1,3 @@
 from sdroptim.mpi_role import *
-from sdroptim.searching_strategies import check_stepwise_available, get_argparse
+from sdroptim.searching_strategies import check_stepwise_available, get_argparse, retrieve_model
 from sdroptim import PythonCodeModulator
