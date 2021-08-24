55112c3b54db50420412e60cfa09e9434bc27c80
jclee0333 <jclee0333@gmail.com>
2021-08-24 11:02:00 +0900

    bug fix
    exceptional cases when n_cols == 1 for GradientFeatureSelector

--- sdroptim/mpi_role.py
+++ sdroptim/mpi_role.py
@@ -2547,7 +2547,7 @@ def model_score(params, job_to_do, dataset, labels, hparams):
     features = X_train.columns.tolist()
     target = target_col
     ################################################
-    if wrapper == "GradientFeatureSelector":
+    if (wrapper == "GradientFeatureSelector") and (n_cols > 1):
         from nni.algorithms.feature_engineering.gradient_selector import FeatureGradientSelector
         gfs_params={}
         gfs_params['learning_rate']=def_hparams['learning_rate']
