642e7daeb92b7e0aef6911709b2633f5a6471641
jclee0333 <jclee0333@gmail.com>
2021-08-27 15:59:17 +0900

    0.2.2 update

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ with open('README.md', 'r', 'utf-8') as f:
 
 setup(
     name             = 'sdroptim',
-    version          = '0.2.0',
+    version          = '0.2.2',
     packages         = find_packages(),
-    description      = 'Hyperparameter Optimization for KISTI Science Data Repository (for slurm job library)',
+    description      = 'Automated Machine Learning Library for KISTI Science Data Repository (for slurm job library)',
     long_description = readme,
     long_description_content_type = 'text/x-rst',
     license          = '',
@@ -19,19 +19,21 @@ setup(
     url              = 'https://github.com/jclee0333/sdroptim',
     #download_url     = 'Git에 저장된 whl 배포용 압축파일',
     install_requires = [
-                        'featuretools',
-                        'nni',
+                        'featuretools', # using for feature synthesis and filtering
+                        'nni', # using for feature selection tools (wrapper-based)
                         'optuna==2.0.0',
                         'mpi4py',
                         'sqlalchemy',
                         'sqlalchemy-utils',
                         'psycopg2-binary',
-                        'easydict',
                         'astunparse',
                         'numpy',
                         'plotly',
-                        'requests',
-                        'pandas',],
+                        'pandas',
+                        'pyarrow', # added for featuretools serialization
+                        'fastparquet',# added for featuretools serialization
+                        'gpuinfo' # added for automatic gpu usage check
+                        ], 
     classifiers      = ['Programming Language :: Python :: 3.6',
                         'Intended Audience :: Korea Institute of Science and Technology Information',
                         'License :: MIT License']
