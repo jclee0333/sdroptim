876944f81e805cbc24f4f2a4cb54065aa636482b
jclee0333 <jclee0333@gmail.com>
2021-10-19 15:45:00 +0900

    minor bug fix
    auto max_depth correction by relationships

--- sdroptim/mpi_role.py
+++ sdroptim/mpi_role.py
@@ -1137,6 +1137,7 @@ def AutoFeatureGeneration(datasetlist, methods, gui_params, current_group_no):
             variable_types=vtypes)
     #################################### 2. Add Relationships
     # check all relationships
+    relationships_flag = False
     if "autofe_system_attr" in gui_params:
         if 'relationships' in gui_params['autofe_system_attr']:
             if len(gui_params['autofe_system_attr']['relationships'])>0:
@@ -1150,16 +1151,18 @@ def AutoFeatureGeneration(datasetlist, methods, gui_params, current_group_no):
                 if relationships:
                     #print(relationships)
                     es = es.add_relationships(relationships)
+                    relationships_flag = True
     ##################################### 3. Do Deep Feature Synthesis
     # element-wise calculation is too big to compute (make limitation on the automate process) # 2021-09-02
     #element_wise_methods = ['and','or','add_numeric','divide_numeric','substract_numeric', 'modulo_numeric','multiply_boolean']
     #n_element_wise = len([x for x in methods[0]+methods[1] if x in element_wise_methods])
     #max_depth = 2 if n_element_wise<2 else 1
+    max_depth = 2 if relationships_flag else 1
     fm, features = ft.dfs(entityset=es, target_entity=os.path.basename(datasetlist[0][0]['filepath']),
                           agg_primitives=methods[0],
                           trans_primitives=methods[1],
                           where_primitives=[], seed_features=[],
-                          max_depth=2, verbose=0)
+                          max_depth=max_depth, verbose=0)
     ### fix index range
     if base_index_has_generated:
         fm.index.name = base_index_name
